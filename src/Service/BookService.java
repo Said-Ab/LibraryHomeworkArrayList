ce6d8869b21451ef08fc1f885243aaa9186ecff7
admin <saidsadyk2907@gmail.com>
2024-11-18 12:23:47 +0600

    Initial commit

---
+++ src/Service/BookService.java
@@ -0,0 +1,15 @@
+package Service;
+
+
+import models.Book;
+
+import java.util.ArrayList;
+
+public interface BookService {
+    String saveBook(Long libraryId, Book book);
+    ArrayList<Book>getAll(Long libraryId);
+    Book getBookById(Long libraryId,Long bookId);
+    String deleteBook(Long libraryId,Long bookId);
+    void clearBookByLibraryId(Long libraryId);
+}
+
---
+++ src/database/Database.java
@@ -0,0 +1,12 @@
+package database;
+
+import models.Library;
+import models.Reader;
+
+
+import java.util.ArrayList;
+
+public class Database {
+    public static ArrayList<Library>libraries=new ArrayList<>();
+    public static ArrayList<Reader>readers=new ArrayList<>();
+}
